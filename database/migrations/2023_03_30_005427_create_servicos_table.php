71e2ff14be8c5cb18217339f4e04ab4258e57b8b
victorlabos <labosvictor1@gmail.com>
2023-03-29 22:35:32 -0300

    cu

---
+++ database/migrations/2023_03_30_005427_create_servicos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('servico');
+            $table->string('prestador');
+            $table->string('descricao');
+            $table->string('valor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
