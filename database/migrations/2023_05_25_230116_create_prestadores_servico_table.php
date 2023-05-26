9fa4c58eb407719c754eaa4fac1df4fe02fb6910
victorlabos <labosvictor1@gmail.com>
2023-05-25 21:57:17 -0300

    25/06

---
+++ database/migrations/2023_05_25_230116_create_prestadores_servico_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestadores_servico', function (Blueprint $table) {
+            $table->id();
+            $table->integer('servico',200);
+            $table->integer('prestadores',200);
+            $table->string('valor');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('cep');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestadores_servico');
+    }
+};
--- resources/views/administrador/create.blade.php
+++ resources/views/administrador/create.blade.php
@@ -1,2 +1,54 @@
 @extends('layout.admin')
 
+@section('conteudo')
+    <form action="{{ route('prestadores.store') }}" method="post" class="mb-3">
+
+        @csrf
+        <h1 class="h3 mb-3 fw-normal">Cadastro do Prestador</h1>
+
+        <div class="form-floating mb-3">
+            <input type="text" name="nome" value="{{ old('nome') }}" class="form-control" placeholder="">
+            <label>Nome:</label>
+        </div>
+
+        @error('nome')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <div class="form-floating mb-3">
+            <input type="email" name="email" value="{{ old('email') }}" class="form-control" placeholder="">
+            <label>E-mail:</label>
+        </div>
+
+        @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-floating mb-3">
+                    <input type="password" name="senha" class="form-control" placeholder="">
+                    <label>Senha:</label>
+                </div>
+
+                @error('senha')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="col-6">
+                <div class="form-floating mb-3">
+                    <input type="password" name="senha_confirmation" class="form-control" placeholder="">
+                    <label>Confirmar senha:</label>
+                </div>
+
+                @error('senha_confirmation')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-success" type="submit">Cadastrar</button>
+    </form>
+
+    <a href="{{ route('administrador.index') }}"><button class="w-100 btn btn-lg btn-warning" type="button">Voltar</button></a>
+@endsection
