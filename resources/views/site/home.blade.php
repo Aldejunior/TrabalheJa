7059e764f24968176534531fb095fa9b16ddada7
Aldejunior <Aldejunioroliveira@gmail.com>
2023-06-06 20:18:53 -0300

    sss

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -44,7 +44,7 @@
                             <a class="navbar-brand"></a>
                             <form class="d-flex" role="search">
                                 <input class="form-control me-2" type="search" placeholder="🔍" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Pesquisa</button>
+                                <button class="btn btn-outline-success" type="submit" >Pesquisa</button>
                             </form>
                         </div>
                     </nav>
